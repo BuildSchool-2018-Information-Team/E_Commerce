c6dd6577d890eae7b9a48be763731106b1bd6e3c
hi <hi@DESKTOP-T18BI0L>
2018-05-10 15:28:12 +0800

    單元測試TestAll

---
+++ CommerceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CommerceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CommerceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9204bbf9-8685-4fad-902d-4ceb1a5b5d6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CommerceTest/TestGetAll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using BuildSchool.MvcSolution.OnlineStore.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommerceTest
+{
+    [TestClass]
+    public class TestGetAll
+    {
+        [TestMethod]
+        public void Test_GetAll_Category()
+        {
+            var repository = new CategoryRepository();
+            var category = repository.GetAll();
+            Assert.IsTrue(category.Count() == 0);
+        }
+        [TestMethod]
+        public void Test_GetAll_Product()
+        {
+            var repository = new ProductRepository();
+            var product = repository.GetAll();
+            Assert.IsTrue(product.Count() == 0);
+        }
+    }
+}
+
