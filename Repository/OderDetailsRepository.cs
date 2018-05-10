97c8b6afde7aadb1ebea07e3eaa2f142d087cbad
hi <hi@DESKTOP-T18BI0L>
2018-05-10 17:46:19 +0800

    新增 orders orderDetails 的reository

---
+++ Repository/OderDetailsRepository.cs
@@ -0,0 +1,121 @@
+﻿using BuildSchool.MvcSolution.OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildSchool.MvcSolution.OnlineStore.Repository
+{
+    public class OderDetailsRepository
+    {
+        public void Create(OrderDetails model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "INSERT INTO OrderDetails VALUES (@OrderID, @ProductFormatID, @Quantity, @UnitPrice)";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+            command.Parameters.AddWithValue("@ProductFormatID", model.ProductFormatID);
+            command.Parameters.AddWithValue("@Quantity", model.Quantity);
+            command.Parameters.AddWithValue("@UnitPrice", model.UnitPrice);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Update(OrderDetails model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "INSERT INTO OrderDetails VALUES (@OrderID, @ProductFormatID, @Quantity, @UnitPrice)";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+            command.Parameters.AddWithValue("@ProductFormatID", model.ProductFormatID);
+            command.Parameters.AddWithValue("@Quantity", model.Quantity);
+            command.Parameters.AddWithValue("@UnitPrice", model.UnitPrice);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Delete(OrderDetails model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "DELETE FROM OrderDetails WHERE OrderID = @OrderID";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public OrderDetails FindById(int OrderID)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "SELECT * FROM OrderDetails WHERE OrderID = @OrderID";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", OrderID);
+
+            connection.Open();
+
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var orderDetails = new OrderDetails();
+
+            while (reader.Read())
+            {
+                orderDetails.OrderID = (int)reader.GetValue(reader.GetOrdinal("OderID"));
+                orderDetails.ProductFormatID = (int)reader.GetValue(reader.GetOrdinal("ProductFormatID"));
+                orderDetails.Quantity = (int)reader.GetValue(reader.GetOrdinal("Quantity"));
+                orderDetails.UnitPrice = (decimal)reader.GetValue(reader.GetOrdinal("UnitPrice"));
+            }
+
+            reader.Close();
+
+            return orderDetails;
+        }
+
+        public IEnumerable<OrderDetails> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "SELECT * FROM OrderDetails";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var orderDetails = new List<OrderDetails>();
+
+            while (reader.Read())
+            {
+                var orderDetail = new OrderDetails();
+                orderDetail.OrderID = (int)reader.GetValue(reader.GetOrdinal("OderID"));
+                orderDetail.ProductFormatID = (int)reader.GetValue(reader.GetOrdinal("ProductFormatID"));
+                orderDetail.Quantity = (int)reader.GetValue(reader.GetOrdinal("Quantity"));
+                orderDetail.UnitPrice = (decimal)reader.GetValue(reader.GetOrdinal("UnitPrice"));
+
+                orderDetails.Add(orderDetail);
+            }
+
+            reader.Close();
+
+            return orderDetails;
+        }
+    }
+}
---
+++ Repository/OrdersRepository.cs
@@ -0,0 +1,131 @@
+﻿using BuildSchool.MvcSolution.OnlineStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace BuildSchool.MvcSolution.OnlineStore.Repository
+{
+    public class OrdersRepository
+    {
+        public void Create(Orders model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "INSERT INTO Orders VALUES (@OrderID, @EmployeeID, @MemberID, @ShipName, @ShipAddress, @ShipPhone, @ShippedDate, @OrderDate, @ReceiptedDate, @Discount, @Status)";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+            command.Parameters.AddWithValue("@EmployeeID", model.EmployeeID);
+            command.Parameters.AddWithValue("@MemberID", model.MemberID);
+            command.Parameters.AddWithValue("@ShipName", model.ShipName);
+            command.Parameters.AddWithValue("@ShipAddress", model.ShipAddress);
+            command.Parameters.AddWithValue("@ShipPhone", model.ShipPhone);
+            command.Parameters.AddWithValue("@ShippedDate", model.ShippedDate);
+            command.Parameters.AddWithValue("@OrderDate", model.OrderDate);
+            command.Parameters.AddWithValue("@ReceiptedDate", model.ReceiptedDate);
+            command.Parameters.AddWithValue("@Discount", model.Discount);
+            command.Parameters.AddWithValue("@Status", model.Status);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        public void Update(Orders model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "UPDATE Orders SET EmployeeID = @EmployeeID, MemberID = @MemberID, ShipName = @ShipName, ShipAddress = @ShipAddress, ShipPhone = @ShipPhone, ShippedDate = @ShippedDate, OrderDate=@OrderDate, ReceiptedDate=@ReceiptedDate, Discount=@Discount, Status = @Status, OrderID=@OrderID ";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+            command.Parameters.AddWithValue("@EmployeeID", model.EmployeeID);
+            command.Parameters.AddWithValue("@MemberID", model.MemberID);
+            command.Parameters.AddWithValue("@ShipName", model.ShipName);
+            command.Parameters.AddWithValue("@ShipAddress", model.ShipAddress);
+            command.Parameters.AddWithValue("@ShipPhone", model.ShipPhone);
+            command.Parameters.AddWithValue("@ShippedDate", model.ShippedDate);
+            command.Parameters.AddWithValue("@OrderDate", model.OrderDate);
+            command.Parameters.AddWithValue("@ReceiptedDate", model.ReceiptedDate);
+            command.Parameters.AddWithValue("@Discount", model.Discount);
+            command.Parameters.AddWithValue("@Status", model.Status);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+        public void Delete(Orders model)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "DELETE FROM Orders WHERE OrderID = @OrderID";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", model.OrderID);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+        public Orders FindById(int OrderID)
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "select * FROM Orders WHERE OrderID = @OrderID";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@OrderID", OrderID);
+
+            connection.Open();
+
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var properties = typeof(Orders).GetProperties();
+            Orders Order = null;
+
+            while (reader.Read())
+            {
+                Order = new Orders();
+                Order = DbReaderModelBinder<Orders>.Bind(reader);
+
+            }
+            reader.Close();
+
+            return Order;
+        }
+        public IEnumerable<Orders> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(
+                "data source=.; database=Commerce; integrated security=true");
+            var sql = "SELECT * FROM Orders";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+
+            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var properties = typeof(Orders).GetProperties();
+            var Orders = new List<Orders>();
+
+            while (reader.Read())
+            {
+                var Order = new Orders();
+                Order = DbReaderModelBinder<Orders>.Bind(reader);
+                Orders.Add(Order);
+            }
+            reader.Close();
+
+            return Orders;
+        }
+    }
+}
